4c88ae51077e7d330ca9030f479f7b38cc2ddddb
dhananjaybakore <dhananjaybakore@gmail.com>
2017-05-04 17:22:16 -0400

    Solution Problem 14 with Cache

--- src/main/java/org/bakore/Problem14.java
+++ src/main/java/org/bakore/Problem14.java
@@ -7,35 +7,74 @@ import java.util.ListIterator;
 
 public class Problem14 {
 
-    public static void main(String[] args){
+    private static final int SIZE=1000000;
+    private static int[] cache = initializeEmptyArray();
+
+    public static void main(String[] args) throws Exception{
 
         long startTime = System.nanoTime();
-        Long largestPathPrime=2L;
+        Long largestPathNumber=2L;
         int largestPath=0;
-        for(Long prime=1L ; prime<1000000; prime++){
-            int collatzPathLength = getCollatzPathLength(prime);
+
+        for(Long i=1L ; i<SIZE; i++){
+            int collatzPathLength = getCollatzPathLengthCache(i);
+            cache[i.intValue()]=collatzPathLength;
             if(collatzPathLength>largestPath){
                 largestPath=collatzPathLength;
-                largestPathPrime=prime;
+                largestPathNumber=i;
             }
-           // System.out.println("Number: "+ prime + " collatzPathLength: "+ collatzPathLength);
+            //System.out.println("Number: "+ i + " collatzPathLength: "+ collatzPathLength);
         }
         long endTime = System.nanoTime()-startTime;
-        System.out.println("Largest Number: "+ largestPathPrime + " Largest Path: "+ largestPath + "With Time: "+ endTime/1000000);
+        System.out.println("Largest Number: "+ largestPathNumber + " Largest Path: "+ largestPath + "With Time: "+ endTime/1000000);
 
     }
 
-    private static int getCollatzPathLength(Long number) {
+    private static int getCollatzPathLengthCache(Long number){
         int path=1;
-        while(number!=1){
+        while(number!=1L){
+            int pathInCache = getCacheValue(number);
+            if(pathInCache!=-1){
+                path+=pathInCache;
+                number=1L;
+            }else{
+                if(number%2==0){
+                    number = number/2;
+                }else{
+                    number = 3* number+1;
+                }
+                path++;
+            }
+        }
+        return  path;
+    }
+
+    private static int getCollatzPathLength(Long number){
+        int path=1;
+        while(number!=1L){
             if(number%2==0){
                 number = number/2;
             }else{
                 number = 3* number+1;
             }
             path++;
+
         }
         return  path;
     }
+    private static int getCacheValue(Long number) {
+       if(number>=SIZE){
+            return -1;
+        }
+        return cache[number.intValue()];
+    }
 
+
+    private static int[] initializeEmptyArray() {
+        int [] arr = new int[SIZE];
+        for(int i=0; i<SIZE; i++){
+            arr[i]=-1;
+        }
+        return arr;
+    }
 }
